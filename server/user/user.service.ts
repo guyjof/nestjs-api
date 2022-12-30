178356c75af21d8100b1e0984a91a38576a756bb
guyjof <guyjofre02@gmail.com>
2022-12-30 21:39:49 +0200

    feat: added user feature

---
+++ server/user/user.service.ts
@@ -0,0 +1,24 @@
+import { PrismaService } from '../prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { EditUserDto } from './dto';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async editUser(userId: number, dto: EditUserDto): Promise<User> {
+    const user = await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+
+    delete user.hash;
+
+    return user;
+  }
+}
